f0af5bd52815df135b55fd195520621c2709e800
Andy Royle <ajroyle@gmail.com>
2014-08-01 12:14:18 +0100

    convert module to a plugin

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-module.exports = require('./lib/auto-reg');
+var autoReg = require('./lib/auto-reg');
+
+exports.register = function (plugin, options, next) {
+
+    plugin.log(["debug"], "registering routes from " + options.directory + "...");
+
+    autoReg.register(plugin, options.directory);
+
+    next();
+};
+
+exports.register.attributes = {
+    pkg: require('./package.json')
+};
---
+++ tests/test-routes/handle-exceptions/error.js
@@ -0,0 +1,3 @@
+module.exports.routes = function(server){
+    throw new Error('ohes noes i r teh borked');
+};
