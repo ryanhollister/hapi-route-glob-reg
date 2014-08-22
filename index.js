8bd8467f0710cb127740385a78f085fb2cdfaee4
Andy Royle <ajroyle@gmail.com>
2014-08-22 14:28:15 +0100

    fix log statement to use correct tag

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var autoReg = require('./lib/auto-reg');
 
 exports.register = function (plugin, options, next) {
 
-    plugin.log(["debug"], "registering routes from " + options.directory + "...");
+    plugin.log(["routes"], "registering routes from " + options.directory + "...");
 
     autoReg.register(plugin, options.directory);
 
