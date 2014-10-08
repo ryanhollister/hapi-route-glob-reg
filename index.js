7726874b57faf3b18b84515b6d58ee59d37ac89a
Steve Higgs <shiggs@opentable.com>
2014-10-08 16:30:32 +0100

    send errors through next which should then bomb out the app

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 var autoReg = require('./lib/auto-reg');
 
 exports.register = function (plugin, options, next) {
-
     plugin.log(["routes"], "registering routes from " + options.directory + "...");
-
-    autoReg.register(plugin, options.directory);
-
-    next();
+    autoReg.register(plugin, options.directory, next);
 };
 
 exports.register.attributes = {
--- tests/tests.js
+++ tests/tests.js
@@ -14,39 +14,41 @@ describe('auto-reg tests', function(){
     });
 
     it('should scan the directory', function(){
-        autoReg.register(plugin, './tests/test-routes/simple/');
+        autoReg.register(plugin, './tests/test-routes/simple/', function(err) {});
         r.length.should.eql(1);
     });
 
     it('should invoke the registration method', function(){
-        autoReg.register(plugin, './tests/test-routes/simple/');
+        autoReg.register(plugin, './tests/test-routes/simple/', function(err) {});
         r[0].path.should.eql('/my/test/route');
     });
 
     it('should search recursively', function(){
-        autoReg.register(plugin, './tests/test-routes/recursive/');
+        autoReg.register(plugin, './tests/test-routes/recursive/', function(err) {});
         r.length.should.eql(2);
     });
 
     it('should ignore files without a .js extension', function(){
-        autoReg.register(plugin, './tests/test-routes/ignore-non-js-files/');
+        autoReg.register(plugin, './tests/test-routes/ignore-non-js-files/', function(err) {});
         r.length.should.eql(1);
         r[0].path.should.eql('/my/test/route');
     });
 
     it('should only invoke modules that are valid', function(){
-        autoReg.register(plugin, './tests/test-routes/ignore-invalid-modules/');
+        autoReg.register(plugin, './tests/test-routes/ignore-invalid-modules/', function(err) {});
         r.length.should.eql(1);
         r[0].path.should.eql('/my/test/route');
     });
 
     it('should cope with a non-existent directory', function(){
-        autoReg.register(plugin, './tests/test-routes/notexist/');
+        autoReg.register(plugin, './tests/test-routes/notexist/', function(err) {});
         r.length.should.eql(0);
     });
 
     it('should handle errors during registration', function(){
-        autoReg.register(plugin, './tests/test-routes/handle-exceptions/');
+        var error = null;
+        autoReg.register(plugin, './tests/test-routes/handle-exceptions/', function(err) { error = err; });
         r.length.should.eql(0);
+        should.exist(error);
     });
 });
