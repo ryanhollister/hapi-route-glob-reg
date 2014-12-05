bfdf099a1e915de0117fc30015949f975d521fcc
Andy Royle <ajroyle@gmail.com>
2014-12-05 08:55:15 +0000

    rename plugin to server to reflect v8.x syntax

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var autoReg = require('./lib/auto-reg');
 
-exports.register = function (plugin, options, next) {
-    plugin.log(["routes"], "registering routes from " + options.directory + "...");
-    autoReg.register(plugin, options.directory, next);
+exports.register = function (server, options, next) {
+    server.log(["routes"], "registering routes from " + options.directory + "...");
+    autoReg.register(server, options.directory, next);
 };
 
 exports.register.attributes = {
